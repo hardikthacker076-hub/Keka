5fa5a7d67c56b102881ffcbd7e9b7bcb2e815123
Hardik Thacker <hardikthacker@Hardik-Thacker-LT.local>
2026-03-03 17:41:36 +0530

    clean up debugging files

--- test_sum.js
+++
@@ -1,8 +0,0 @@
-
-const now = new Date("2026-03-03T17:21:00");
-// They worked 3h 33m out of 8h (meaning 4h 27m left to work)
-const leftMinutes = (4 * 60) + 27;
-
-const outDateObj = new Date(now.getTime() + (leftMinutes * 60000));
-console.log("Expected Logoff if calculated right now:", outDateObj.getHours() + ":" + outDateObj.getMinutes());
-
